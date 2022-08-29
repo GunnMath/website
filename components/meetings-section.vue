a8949d4773fc1cc05387d7b093398e7731039632
Saumya Singhal <65318685+saumyasinghal747@users.noreply.github.com>
2022-08-29 07:18:33 -0700

    remove maximum width from prose

--- components/meetings-section.vue
+++ components/meetings-section.vue
@@ -5,7 +5,7 @@
     <div class="w-full  bg-slate-800 rounded-2xl py-6 px-8 shadow-lg shadow-2xl">
         <h3 class="font-bold text-2xl mb-4">{{latestMeeting.title}}</h3>
         
-              <content-renderer  class="prose prose-invert text-white/80 prose-sm leading-tight" :value="latestMeeting"/>
+              <content-renderer  class="prose prose-invert text-white/80 prose-sm leading-tight max-w-none" :value="latestMeeting"/>
     </div>
     <div class="">
         <a href="/meetings" class="py-2 px-3 rounded-xl cursor-pointer font-bold text-lg bg-brand text-white float-right flex flex-row items-center gap-2">All Meetings <icon-right class="fill-current h-4 w-4" /></a>
